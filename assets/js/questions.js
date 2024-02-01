ad0cbbe7eb2bd46b4ff39fc472121769468f1508
Amanda Fahy <amandafahy@ymail.com>
2024-02-01 03:30:56 +0000

    Add javascript for start, information box, quiz container and score.

---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+//Quiz Questions and Options array
+
+const questionsArray = [{
+
+        question: "What is the longest river in Ireland?",
+        options: ["The Liffey", "The Corrib", "The Shannon", "The Lee"],
+        correct: "The Shannon",
+    },
+    {
+        question: "What animal did St Patrick drive out of Ireland ?",
+        options: ["Spiders", "Snakes", "Dragons", "Bears"],
+        correct: "Snakes",
+    },
+    {
+
+        question: "What is the name of  Irelands Capital city?",
+        options: ["Cork", "Galway", "Dublin", "Limerick"],
+        correct: "Dublin",
+    },
+    {
+        question: "Who is the President of Ireland ?",
+        options: ["Michael D Higgins", "Leo Varadkar", "Marty Morrissey", "Bono"],
+        correct: "Michael D Higgins",
+    },
+    {
+
+        question: "What is  Irelands smallest county?",
+        options: ["Louth", "Armagh", "Clare", "Wexford"],
+        correct: "Louth",
+    },
+    {
+        question: "What musical instrument does Ireland have as one of its official symbols ?",
+        options: ["Harp", "Piano", "Drums", "Fiddle"],
+        correct: "Harp",
+    },
+    {
+
+        question: "What does  “sláinte” mean?",
+        options: ["Cheers", "Hello", "Good Luck", "Goodbye"],
+        correct: "Cheers",
+    },
+    {
+        question: "Which of these is commonly known in Ireland as “the black stuff”? ?",
+        options: ["Guinness", "Sausages", "Coal", "Money"],
+        correct: "Guinness",
+    },
+    {
+
+        question: "Kissing the Blarney Stone is said to bestow which talent upon the one kissing the stone?",
+        options: ["Gift of the gab", "Singing", " Riverdancing", "Knowledge"],
+        correct: "Gift of the gab",
+    },
+    {
+        question: "How many leaves does a shamrock have? ?",
+        options: ["2", "4", "6", "3"],
+        correct: "3",
+    },
+
+];
---
+++ assets/js/script.js
@@ -0,0 +1,208 @@
+//Selecting required elements
+const quizContainer = document.getElementById("quiz-container");
+const nextBtn = document.getElementById("next-button");
+const displayContainer = document.getElementById("display-container");
+const scoreContainer = document.querySelector(".score-container");
+const restart = document.getElementById("restart");
+const userScore = document.getElementById("user-score");
+const startScreen = document.querySelector(".start-screen");
+const startButton = document.getElementById("start-button");
+const infoBox = document.querySelector(".info-box");
+const exitBtn = infoBox.querySelector(".info-buttons .exit-btn");
+const playBtn = infoBox.querySelector(".info-buttons .play-btn");
+let timeLeft = document.querySelector(".time-left");
+let showNextQuestion = document.querySelector(".next-question");
+let countOfQuestion = document.querySelector(".number-of-question");
+let resultImage = document.getElementById("result-image")
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+// creating the new div tags for icons
+const tickIcon = '<div class="icon-tick"><i class="fa-regular fa-circle-check"></i></div>';
+const crossIcon = '<div class="icon-cross"><i class="fa-regular fa-circle-xmark"></i></div>';
+
+
+//Restart Quiz
+restart.addEventListener("click", () => {
+    startGame();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+});
+
+function showResult() {
+    console.log('result')
+    //user score
+    userScore.innerHTML = "Your score is " + scoreCount + " out of " + questionCount;
+    if (scoreCount === 0 && scoreCount <= 2) {
+        userScore.innerHTML += "<p>Not Good</p>";
+    } else if (scoreCount === 3 && scoreCount <= 5) {
+        userScore.innerHTML += "<p>I think you need practice</p>";
+    } else if (scoreCount === 6 && scoreCount <= 8) {
+        userScore.innerHTML += "<p>I think you need more practice</p>";
+    } else {
+        userScore.innerHTML += "<p>Excellent</p>";
+    }
+}
+//Next Button
+nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+        //increment questionCount
+        questionCount++;
+        //if last question
+        if (questionCount == questionsArray.length) {
+            //hide question container and display score
+            displayContainer.classList.add("hide");
+            scoreContainer.classList.remove("hide");
+            showResult()
+        } else {
+            //display questionCount
+            countOfQuestion.innerHTML =
+                questionCount + 1 + " of " + questionsArray.length + " Question";
+            //display quiz
+            quizDisplay(questionCount);
+            count = 11;
+            clearInterval(countdown);
+            timerDisplay();
+        }
+
+    }));
+
+// Timer
+const timerDisplay = () => {
+    countdown = setInterval(() => {
+        count--;
+        timeLeft.innerHTML = `${count}s`;
+        if (count == 0) {
+            //when countdown reaches 0 clearInterval and go to next question
+
+            clearInterval(countdown)
+            console.log('time-up')
+            displayNext();
+        }
+    }, 1000);
+};
+//Display quiz
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".quiz-container-mid");
+    //Hide other cards
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    //display current question card
+    quizCards[questionCount].classList.remove("hide");
+    //Disable Next Button
+    nextBtn.disabled = true;
+};
+
+function showQuestion() {
+    console.log('showQuestion')
+    //Generate quiz
+    for (let i of questionsArray) {
+        //Randomly sort answer options
+        i.options.sort(() => Math.random() - 0.5);
+        //Quiz card creation
+        let div = document.createElement("div");
+        div.classList.add("quiz-container-mid", "hide");
+        //Quiz question number
+        countOfQuestion.innerHTML = 1 + " of " + questionsArray.length + " Question";
+        //Quiz question
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+        div.appendChild(question_DIV);
+        //Answer options
+        div.innerHTML += `
+    <button class="option-div" onclick=" checkAnswer(this)">${i.options[0]}</button>
+     <button class="option-div" onclick=" checkAnswer(this)">${i.options[1]}</button>
+      <button class="option-div" onclick=" checkAnswer(this)">${i.options[2]}</button>
+       <button class="option-div" onclick=" checkAnswer(this)">${i.options[3]}</button>
+    `;
+        quizContainer.appendChild(div);
+        console.log('selectAnswer')
+    }
+}
+
+
+//Check answer function to check if user selected answer option is correct or incorrect.
+function checkAnswer(userAnswer) {
+    console.log('checkAnswer')
+    let userSelectedAnswer = userAnswer.innerText;
+    let question =
+        document.getElementsByClassName("quiz-container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+    //if user clicked answer == correct option stored in object
+    if (userSelectedAnswer === questionsArray[questionCount].correct) {
+        //adding green color to correct selected option
+        userAnswer.classList.add("correct");
+        //Add tick icon to correct selected option
+        userAnswer.insertAdjacentHTML("beforeend", tickIcon);
+        console.log("Correct Answer");
+        scoreCount++;
+    } else {
+        //Add red color to incorrect selected option
+        userAnswer.classList.add("incorrect");
+        //Add cross icon to incorrect selected option
+        userAnswer.insertAdjacentHTML("beforeend", crossIcon);
+        options.forEach((element) => {
+            if (element.innerText == questionsArray[questionCount].correct) {
+                element.classList.add("correct");
+                console.log("Incorrect Answer");
+
+            }
+        });
+    }
+    //clear interval(stop timer)
+    clearInterval(countdown);
+    //disable all options
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+    //Next Button enabled.
+    nextBtn.removeAttribute('disabled');
+}
+//initial setup
+function startGame() {
+    console.log('startGame')
+    quizContainer.innerHTML = "";
+    // Randomly sort questions
+    questionsArray.sort(() => Math.random() - 0.5);
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    showQuestion();
+    quizDisplay(questionCount);
+    timerDisplay()
+}
+
+// Play Button on Information Box popup, on click brings up quiz container. 
+playBtn.onclick = () => {
+    infoBox.classList.remove("active");
+    //Shows Quiz container
+    quizContainer.classList.add("quiz-container");
+    //Hides start screen.
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    console.log('play')
+    startGame();
+}
+
+// Exit button on Information box popup, on click exits.
+exitBtn.onclick = () => {
+    //Wll hide rules.
+    infoBox.classList.remove("active");
+    console.log('exit')
+}
+//Start Button on click brings up quiz playing information.
+startButton.onclick = () => {
+    console.log('running')
+    //Shows information  box
+    infoBox.classList.add("active");
+}
+//Hide quiz and display start screen.
+window.onload = () => {
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
+};
